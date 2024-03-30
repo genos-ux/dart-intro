7dde6a508a2fa5fdc0f7d959327b89094d6cb8eb
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-03-30 08:39:01 +0000

    control flow

---
+++ control_flow.dart
@@ -0,0 +1,23 @@
+// Dart Control Flow Assignment
+
+
+// Create a Dart program that prompts the user for a number and then prints a message to the console based on the following criteria:
+
+// If the number is greater than 10, print "Your number is greater than 10"
+// If the number is less than 10, print "Your number is less than 10"
+// If the number is equal to 10, print "Your number is equal to 10"
+
+void main(){
+  var user_number = 10;
+
+  if(user_number > 10){
+    print("Your number is greater than 10");
+  }
+  else if(user_number < 10){
+    print("Your number is less than 10");
+  }
+  else{
+    print("Your number is equal to 10");
+  }
+}
+
